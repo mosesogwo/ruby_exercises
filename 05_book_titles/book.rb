001a052c88cbe58334894e1023ae1863bc235926
Moses Ogwo <mosesogwo@gmail.com>
2019-03-01 19:40:21 +0100

    All solutions provided

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,27 @@
 class Book
-# write your code here
+    # write your code here
+    
+    
+    def title 
+        @title
+    end
+    
+    def title=(name)
+        no_capital = ['and', 'the', 'a', 'an', 'in', 'of']
+        name = name.split(" ")
+        new_name = []
+        name.each do |i|
+            if no_capital.include?(i) == false
+                new_name << i.capitalize
+            else
+                new_name << i
+            end
+            new_name
+        end
+        if no_capital.include?(new_name[0])
+            new_name[0] = new_name[0].capitalize
+            new_name
+        end
+         @title = new_name.join(" ")
+    end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
   #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=seconds
+    @seconds = seconds
+  end
+
+  def time_string
+    Time.at(seconds - 3600).strftime("%H:%M:%S")
+  end
+
 end
